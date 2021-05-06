3655953727705bae67b032dfe004b3f322fc315c
Tuhh16 <arthur.alves.lima@live.com>
2021-05-05 22:41:48 -0300

    Ajustes no header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
                     <meta property="og:description" content="Aplicação que exibe varios podcasts da Rocketseat via Api" />
                     <meta property="og:type" content="website" />
                     <meta property="og:url" content="https://podcastnextjs.vercel.app" />
-                    <meta property="og:image" content="/app-preview.png" />
+                    <meta property="og:image" content="https://podcastnextjs.vercel.app/app-preview.png" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
                     <link rel="shortcut icon" href="/favicon.png" type="image/png" />
