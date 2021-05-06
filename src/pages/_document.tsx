7e92e7fc9af0770c6ec6626b073b0fb64984484b
Tuhh16 <arthur.alves.lima@live.com>
2021-05-06 15:04:35 -0300

    imagens

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
                     <meta property="og:description" content="Aplicação que exibe os episódios dos podcasts da Rocketseat onde cada um tem uma pagina interna dando mais detalhes do episódio." />
                     <meta property="og:type" content="website" />
                     <meta property="og:url" content="https://podcastnextjs.vercel.app" />
-                    <meta property="og:image" content="https://podcastnextjs.vercel.app/app-preview.png" />
+                    <meta property="og:image" content="https://podcastnextjs.vercel.app/screenshot/podcastr-home-preview.jpg" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
                     <link rel="shortcut icon" href="/favicon.png" type="image/png" />
