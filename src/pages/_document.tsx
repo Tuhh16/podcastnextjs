587c6947c44c38b0f304a0e485ea0a6cfa88ae22
Tuhh16 <arthur.alves.lima@live.com>
2021-05-05 22:29:21 -0300

    Ajustes no header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,14 @@ export default class MyDocument extends Document {
         return(
             <Html>
                 <Head>
+                    <meta property="og:title" content="Podcastr" key="Podcastr" />
+                    <meta property="og:description" content="Aplicação que exibe varios podcasts da Rocketseat via Api" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="https://podcastnextjs.vercel.app" />
+                    <meta property="og:image" content="/app-preview.png" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
-                
-                    <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
+                    <link rel="shortcut icon" href="/favicon.png" type="image/png" />
                 </Head>
                 <body>
                     <Main />
